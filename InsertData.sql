7dd853e84f58065022df67df7ec0e18d86d38e9c
PGSwart <72019632+PGSwart@users.noreply.github.com>
2021-03-25 16:27:27 +0200

    Add files via upload

---
+++ InsertData.sql
@@ -0,0 +1,72 @@
+ use DBpremier_service_solutions
+  go
+
+  Insert into Business
+  values('Pandalytics','0833909155','833  Visser St'),
+	('Crescent','0841921026','208  Rus St'),
+	('Blue Space','0838170187','781  Daffodil Dr'),
+	('App Droid','0822234185','1040  President St'),
+	('Audiotrix','0843555736','290  Wolmarans St'),
+	('Source Code','0849686357','2077  Wattle St'),
+	('Debugging Code','0824918427','2279  Dickens St'),
+	('TerminalTrend','0839438190','23  Prospect St'),
+	('Data Source','0826649411','1951  Ireland St'),
+	('Quest Labs','0827733245','253  Kort St')
+
+  go
+
+  Insert into Employees
+  values ('Juan Ludick','ludickjuan@gmail.com','0845879611','51  Bo Meul St',''),
+	('Kurt Nobrain','exleadsingerofaband@gmail.com','0123544201','720  Barlow Street',''),
+	('Martin Leather','deadactivistlol123@gmail.com','0554689212','196  President St',''),
+	('Sia Furler','theunkonownartistwithwhitehair@gmail.com','0346595468','2424  Bodenstein St',''),
+	('Corey Taylor','ihavetwobandslol@gmail.com','03216496854','145  Akasia St',''),
+	('Sean Mendalev','imdatingalatinafromagirlgroupthatbrokeup@gmail.com','0369459865','1718  Amos St',''),
+	('Azure Auraelia','bruhwhyisthismyname@gmail.com','0996641379','2298  Plane St',''),
+	('Danielle Cannot','yahoo@gmail.com','0465269852','1640  Glyn St',''),
+	('James Moriaty','gotkilkledbymynemisislol@gmail.com','0776653321','1128  North Street',''),
+	('Sherlock Holmies','ikilledmynemisislol@gmail.com','0699666993','2312  Bo Meul St',''),
+	('Cindy Ludick','Cindyludick@gmail.com','0845879611','833  Visser St','')
+
+	go
+
+	Insert into Clients 
+	Values
+	('Tiaan','Jacobs',,'U','tiaanjacobs@gmail.com','',),
+	('Etienne','Hores',,'U','etiennehores@gmail.com','',),
+	('Jason','Boniface',,'M','jasonboniface@gmail.com','',),
+	('Robert','Patterson',,'M','robertpatterson@gmail.com','',),
+	('Regina','Wajowski',,'F','reginawajowski@gmail.com','',),
+	('Willy','Stroker',,'M','willystroker@gmail.com','',),
+	('Jeanette','Doe',,'F','jeanettedoe@gmail.com','',),
+	('Lovemore','Sibanda',,'F','lovemoresibanda@gmail.com','',),
+	('Charles','Beckham',,'M','charlesbeckham@gmail.com','',),
+	('Tina','Turner',,'F','tinaturner@gmail.com','',),
+	('Ma','Madea',,'U','mamadea@gmail.com','',),
+	('George','Bush',,'M','george@gmail.com','',),
+	('Trevor','Jones',,'M','trevorjones@gmail.com','',),
+	('Nina','Roberts',,'F','ninaroberts@gmail.com','',),
+	('Johan','Ramaphosa',,'M','johanramaphosa@gmail.com','',),
+	('Christopher','Robin',,'M','christopher@gmail.com','',),
+	('Winnie','Pooh',,'U','winniepooh@gmail.com','',),
+	('Kim','Jonson',,'M','kimjonson@gmail.com','',),
+	('Abe','Helfer',,'F','abehelfer@gmail.com','',),
+	('Ghengis','Khan',,'M','ghengis@gmail.com','',),
+	('Atilla','Hunterson',,'F','atilla@gmail.com','',),
+	('Vlad','Tepesche',,'U','vladtepesche@gmail.com','',),
+	('Joan','Arch',,'M','joanarch@gmail.com','',),
+	('Dorothy','Oz',,'F','dorothyoz@gmail.com','',),
+	('Belle','Delphine',,'F','belledelphine@gmail.com','',),
+	('Yoda','Layhehoo',,'U','yodalayhehoo@gmail.com','',),
+	('Freddie','Fasbear',,'M','freddie@gmail.com','',),
+	('Stephan','Notwakin',,'M','stephan@gmail.com','',),
+	('Gabe','Itches',,'F','gabeitches@gmail.com','',),
+	('Fredd','Le Staire',,'M','freddlestaire@gmail.com','',),
+	('Jacob','Zoomer',,'M','jacobzoomer@gmail.com','',),
+	('Fixer','Uupper',,'U','fixeruupper@gmail.com','',),
+	('Ben','Lauden',,'M','benlauden@gmail.com','',),
+	('El','Quisha',,'F','elquisha@gmail.com','',),
+	('Ei','Cys',,'F','eicys@gmail.com','',)
+
+	go
+	insert into 
