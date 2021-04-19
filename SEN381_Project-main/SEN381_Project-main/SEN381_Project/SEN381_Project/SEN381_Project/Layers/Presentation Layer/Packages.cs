5006cd84906eddb5ce8bae846950c57350a1cdac
PGSwart <72019632+PGSwart@users.noreply.github.com>
2021-04-19 10:50:52 +0200

    Add files via upload

---
+++ SEN381_Project-main/SEN381_Project-main/SEN381_Project/SEN381_Project/SEN381_Project/Layers/Presentation Layer/Packages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SEN381_Project.Layers.Presentation_Layer
+{
+    public partial class Packages : Form
+    {
+        public Packages()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SEN381_Project-main/SEN381_Project-main/SEN381_Project/SEN381_Project/SEN381_Project/Layers/Presentation Layer/Tickets.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SEN381_Project.Layers.Presentation_Layer
+{
+    partial class Tickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 55);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(862, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(282, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete Record";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(862, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cleint Problem";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1302, 81);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(360, 69);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Assign Employee";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Tickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2713, 1091);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Tickets";
+            this.Text = "Tickets";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+    }
+}
